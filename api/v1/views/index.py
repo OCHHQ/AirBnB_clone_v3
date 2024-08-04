1f6b2b6d2424c686e206e94ef24b25f0df551a67
Collins Enoseje <enosejecollins@gmail.com>
2024-08-04 17:44:33 +0100

    Fix stats endpoint to retrieve correct counts for each object type

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,22 +1,30 @@
 from flask import jsonify
 from api.v1.views import app_views
-
-
-@app_views.route('/status', methods=['GET'], strict_slashes=False)
-def status():
-    return jsonify({"status": "OK"})
-
+from models import storage
 
 @app_views.route('/stats', methods=['GET'], strict_slashes=False)
 def stats():
-    """Retrives the number of each objects by type"""
-
+    """Retrieves the number of each objects by type"""
+    amenities_count = storage.count("Amenity")
+    cities_count = storage.count("City")
+    places_count = storage.count("Place")
+    reviews_count = storage.count("Review")
+    states_count = storage.count("State")
+    users_count = storage.count("User")
+    
+    print("Amenities count:", amenities_count)
+    print("Cities count:", cities_count)
+    print("Places count:", places_count)
+    print("Reviews count:", reviews_count)
+    print("States count:", states_count)
+    print("Users count:", users_count)
+    
     stats = {
-        "amenities": storage.count("Amenity"),
-        "cities": storage.count("City"),
-        "places": storage.count("Place"),
-        "reviews": storage.count("Review"),
-        "states": storage.count("States"),
-        "users": storage.count("Users")
+        "amenities": amenities_count,
+        "cities": cities_count,
+        "places": places_count,
+        "reviews": reviews_count,
+        "states": states_count,
+        "users": users_count
     }
     return jsonify(stats)
